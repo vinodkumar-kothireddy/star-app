9d7c4f2b2e3e64af4290dd21564a8ccc6cf88fbb
vinodkumar-kothireddy <vinodkumar.kothireddy.com>
2025-06-03 02:01:40 +0530

    update Router

--- src/My-app.jsx
+++ src/My-app.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
